3341b01e18d704e2bc68dda526a8ef2db3e9e379
Amelia Ikeda <amelia@dorks.io>
2016-09-05 12:31:08 +0100

    Add more tests for persistent caching (deleting, updating)

--- test/PersistentTest.php
+++ test/PersistentTest.php
@@ -35,4 +35,49 @@ class PersistentTest extends RememberTestCase
 
         $this->assertEquals($group, Group::find(static::ID));
     }
+
+    public function testPersistentCachingWhenDeleting()
+    {
+        Group::create(['id' => static::ID, 'name' => 'Delete Test']);
+
+        $group = Group::find(static::ID);
+
+        $group->delete();
+
+        $cached = Group::find(static::ID);
+
+        $this->assertNull($cached);
+    }
+
+    /**
+     * @expectedException SqlIssuedException
+     */
+    public function testSqlIssuedAfterDeletion()
+    {
+        Group::create(['id' => static::ID, 'name' => 'Delete Test']);
+
+        $group = Group::find(static::ID);
+
+        $group->delete();
+
+        static::$sql = false;
+
+        Group::find(static::ID);
+    }
+
+    /**
+     * @expectedException SqlIssuedException
+     */
+    public function testSqlIssuedAfterUpdating()
+    {
+        Group::create(['id' => static::ID, 'name' => 'Delete Test']);
+
+        $group = Group::find(static::ID);
+
+        $group->update(['name' => 'foobar']);
+
+        static::$sql = false;
+
+        Group::find(static::ID);
+    }
 }
