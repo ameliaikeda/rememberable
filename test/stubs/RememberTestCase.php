d295d3435a1019577ce6536560320169c4366828
Amelia Ikeda <amelia@dorks.io>
2016-09-05 12:30:54 +0100

    Make the base test case abstract

--- test/stubs/RememberTestCase.php
+++ test/stubs/RememberTestCase.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 
-class RememberTestCase extends PHPUnit_Framework_TestCase
+abstract class RememberTestCase extends PHPUnit_Framework_TestCase
 {
     const ID = '00000000-0000-0000-0000-000000000000';
     const USER_KEY = 'rememberable:37e1e4277521620256c524b5598dec2e19a04bfaf581bb9da4f83c3ad4ff7dbb7b03e7db5175eb2f3e507b4f8133fc2837eb392f7a92ce118ebff3a0e72cad9a';
